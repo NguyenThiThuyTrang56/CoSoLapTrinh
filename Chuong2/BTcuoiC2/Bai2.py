8ffacba8e6b747cac150d55472e65bc2c431cc7e
NguyenThiThuyTrang56 <129813110+NguyenThiThuyTrang56@users.noreply.github.com>
2023-04-06 21:52:54 +0700

    create file CoSoLapTrinh

--- Chuong2/BTcuoiC2/Bai2.py
+++ Chuong2/BTcuoiC2/Bai2.py
@@ -2,5 +2,5 @@ import math
 r=int(input("Nhap ban kinh cua duong tron: "))
 Dientich=round(3.14*(r**2),1)
 Chuvi=round(2*3.14*r,1)
-print("Dien tich của duong tron co ban kinh",r,"la =",round(Dientich,1))
-print("Chu vi cua duong tron co ban kinh",r,"la =",round(Chuvi,1))
+print("Dien tich của duong tron co ban kinh",r,"la =",Dientich)
+print("Chu vi cua duong tron co ban kinh",r,"la =",Chuvi)
